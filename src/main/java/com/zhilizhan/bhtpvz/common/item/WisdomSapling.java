2af1cd3e2bda972ffebd8bceb2e793fbc6f07934
Lzc-SuSen <linchenchen36@gmail.com>
2023-12-10 14:52:11 +0800

    树苗优化

--- src/main/java/com/zhilizhan/bhtpvz/common/item/WisdomSapling.java
+++ src/main/java/com/zhilizhan/bhtpvz/common/item/WisdomSapling.java
@@ -3,11 +3,17 @@ package com.zhilizhan.bhtpvz.common.item;
 import com.hungteen.pvz.common.capability.CapabilityHandler;
 import com.hungteen.pvz.utils.PlayerUtil;
 import com.hungteen.pvz.utils.enums.Resources;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class WisdomSapling extends XpSapling {
     public WisdomSapling(Properties properties) {
@@ -28,5 +34,10 @@ public class WisdomSapling extends XpSapling {
             }
         }
         return stack;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, Level level, List<Component> tooltipComponents, TooltipFlag isAdvanced) {
+        tooltipComponents.add(new TranslatableComponent("tooltip.bhtpvz.wisdom_sapling.use").withStyle(ChatFormatting.GREEN));
     }
 }
